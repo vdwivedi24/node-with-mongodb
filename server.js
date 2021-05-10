a1f4e79f2439656a78ff9eb20f14c588171f6bf7
Vikrant Dwivedi <vkrntdwivedi224@gmail.com>
2021-05-10 19:04:02 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const http =  require('http');
+
+const server =  http.createServer((req,res)=>{
+ res.end();
+});
+
+const PORT = 5000;
+
+server.listen(PORT, ()=>  console.log(`Server listening on port ${PORT}`));
