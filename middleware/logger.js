0d596d2412ef0ab59ebd233e25c1773c30092211
Vikrant Dwivedi <vkrntdwivedi224@gmail.com>
2021-05-12 16:08:51 -0400

    Added some basic routes along with morgan middleware

---
+++ middleware/logger.js
@@ -0,0 +1,9 @@
+//@desc Logs request to console
+
+const logger = (req,res,next)=>{
+    req.hello = 'Hello World';
+    console.log('Middleware ran');
+    next();
+}
+
+module.exports = logger;
--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const bootcamp = require('./routes/bootcamps');
+// const logger = require('./middleware/logger');
+const morgan =  require('morgan');
 dotenv.config({
     path:'./config/config.env'
 });
 
 const app = express();
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+// app.use(logger);
+
+app.use('/api/v1/bootcamps', bootcamp);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on part ${PORT}`));
+app.listen(5000, ()=> console.log(`Server running in ${process.env.NODE_ENV} mode on part ${PORT}`));
